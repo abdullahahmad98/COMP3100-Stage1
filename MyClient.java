d1e111f195995e147410df25cf014be38a071d8e
abdullahahmad98 <abdullah.ahmad@students.mq.edu.au>
2023-04-01 14:01:00 +1100

    Changed code

--- MyClient.java
+++ MyClient.java
@@ -1,104 +1,65 @@
 import java.net.*;  
 import java.io.*;  
-
 class MyClient{  
-    public static void main(String args[])throws Exception{  
-    Socket s=new Socket("localhost",50000);  
-    
-     DataInputStream din=new DataInputStream(s.getInputStream());  
-    DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
-    BufferedReader in =new BufferedReader(new InputStreamReader(s.getInputStream()));  
-      
-    	String str = "";
-	
-	
-    	dout.write(("HELO\n").getBytes()); 
-    	dout.flush();
-    	System.out.println("HELO has been sent");
-    	str = in.readLine();
-    	System.out.println("Server says: "+ str);
-    	
-    	dout.write(("AUTH Abdullah\n").getBytes()); 
-    	dout.flush();
-    	System.out.println("Auth has been sent");
-    	str = in.readLine();
-    	System.out.println("Server says: "+ str);
-  
-    	dout.write(("REDY\n").getBytes()); 
-    	dout.flush();
-    	System.out.println("REDY has been sent");
-    	str = in.readLine();
-    	System.out.println("Server says: "+ str);
-    	
-    	//Remove the first 4 words of the server's response.
-    	int index = str.indexOf(" ");
-    	for (int i=0; i<3; i++){
-    		index = str.indexOf(" ", index + 1);
-    	}
-    	String jobn = str.substring(index + 1);
-    	
-    	dout.write(("GETS Capable " + jobn + "\n").getBytes()); 
-    	dout.flush();
-    	System.out.println("GETS has been sent");
-    	str = in.readLine();
-    	System.out.println("Server says: "+ str);
+public static void main(String args[])throws Exception{  
+	Socket s=new Socket("localhost",50000);  //port  
+	DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
+	BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
 
-		index = str.indexOf(" ");
-		int startIndex = index;
-		index = str.indexOf(" ", index + 1);
-		int endIndex = index;
-		String numberServers = str.substring(startIndex + 1, endIndex);
+	String str="",str2="";
+	dout.write(("HELO\n").getBytes());
+	dout.flush(); 
+	str=in.readLine();
+	String username = System.getProperty("user.name");
+	dout.write(("AUTH "+username+"\n").getBytes());
+	dout.flush(); 
+	str=in.readLine();
 
-
-    	dout.write(("OK\n").getBytes()); 
-    	dout.flush();
-    	System.out.println("OK has been sent");
-
-		int numServers = Integer.parseInt(numberServers); 
-		String strArr[] = new String[numServers];
-		String str2dArr[][] = new String[numServers][];
-		for (int i=0; i < numServers; i++) {
-    		str = in.readLine();
-			strArr[i] = str;
-    		System.out.println("Server says: "+ str);
-		}
-		for (int i=0; i < numServers; i++) {
-			str2dArr[i] = strArr[i].split(" ");
-		}
-
-		/*
-		for (int i=0; i < str2dArr.length; i++) {
-			for (int j=0; j < str2dArr[i].length; j++) {
-				System.out.print(str2dArr[i][j] + " ");
+	boolean state = true;
+	int jobID = 0, maxCores = 0, nRecs = 0, n = 0, cores = 0;
+	String maxType="";
+	String[] Array = null;
+	while(!str.equals("NONE")){
+		dout.write(("REDY\n").getBytes());
+		dout.flush();
+		str2=in.readLine();
+		if(state){
+			dout.write(("GETS All\n").getBytes());
+			dout.flush();
+			str=in.readLine();
+			Array = str.split(" ");
+			nRecs = Integer.parseInt(Array[1]);
+			dout.write(("OK\n").getBytes());
+			dout.flush();
+			for(int i=0;i<nRecs;i++){
+				String[] Array2=null;
+				str=in.readLine();
+				Array2 = str.split(" ");
+				cores = Integer.parseInt(Array2[4]);
+				if(cores > maxCores){
+					maxType = Array2[0];
+					maxCores = cores;
+				}
+				if(maxType.equals(Array2[0])) n = Integer.parseInt(Array2[1]);
 			}
-			System.out.println();
+			n++;
+			dout.write(("OK\n").getBytes());
+			dout.flush();
+			str=in.readLine();
+			state = false;
 		}
-		*/
 
-    	dout.write(("OK\n").getBytes()); 
-    	dout.flush();
-    	System.out.println("OK has been sent");
-    	str = in.readLine();
-    	System.out.println("Server says: "+ str);
-  
-    	dout.write(("REDY\n").getBytes()); 
-    	dout.flush();
-    	System.out.println("REDY has been sent");
-    	str = in.readLine();
-    	System.out.println("Server says: "+ str);
-  
-    	dout.write(("SCHD 0 medium 0\n").getBytes()); 
-    	dout.flush();
-    	System.out.println("SCHD has been sent");
-    	str = in.readLine();
-    	System.out.println("Server says: "+ str);
-    	
-    	dout.write(("QUIT\n").getBytes()); 
-    	dout.flush();
-    	System.out.println("Quit has been sent");
-    	str = in.readLine();
-    	System.out.println("Server says: "+ str);
-      
-    dout.close();  
-    s.close();  
-    }}  
+		if(str2.contains("JOBN")){
+			dout.write(("SCHD "+jobID+" "+maxType+" "+jobID%n+"\n").getBytes());
+			dout.flush();
+			jobID++;
+			str=in.readLine();
+		}
+		else str = str2;
+	}
+
+	dout.write(("QUIT\n").getBytes());
+	dout.flush();
+	dout.close(); 
+	s.close();
+}}
