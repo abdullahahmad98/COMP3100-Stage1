ce3495a8d263e8ec31c6874e31b32132402e43d0
abdullahahmad98 <abdullah.ahmad@students.mq.edu.au>
2023-04-03 20:23:00 +1000

    Updated code

--- MyClient.java
+++ MyClient.java
@@ -1,99 +1,88 @@
 import java.net.*;
 import java.io.*;
 
-class MyClient{
-	public static void main(String args[])throws Exception{
-		Socket s=new Socket("localhost",50000);
-		DataOutputStream dout=new DataOutputStream(s.getOutputStream());
-		BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
-	
-		// Send HELO message and receive response
-		String serverResponse = sendAndReceive(dout, in, "HELO\n");
-	
-		// Send AUTH message with user name and receive response
-		String user = System.getProperty("user.name");
-		serverResponse = sendAndReceive(dout, in, "AUTH "+user+"\n");
-	
-		// Initialize variables for scheduling jobs
-		boolean firstTime = true;
-		int jobID = 0, maxCores = 0, nRecs = 0, n = 0, cores = 0;
-		String maxType="";
-		String serverResponse2 = "";
-		String[] jobFields = null;
-		
-		// Loop until there are no more jobs
-		while(!serverResponse.equals("NONE")){
-			// Send REDY message and receive response
-			serverResponse2=sendAndReceive(dout, in, "REDY\n");
-			if(firstTime){
-				// Send GETS All message and receive response
-				serverResponse=sendAndReceive(dout, in, "GETS All\n");
-				jobFields = serverResponse.split(" ");
-				nRecs = Integer.parseInt(jobFields[1]);
-				send(dout, "OK\n");
-				// Find the server type with the maximum cores
-				for(int i=0;i<nRecs;i++){
-					String[] jobFields2=null;
-					serverResponse=receive(in);
-					jobFields2 = serverResponse.split(" ");
-					cores = Integer.parseInt(jobFields2[4]);
-					if(cores > maxCores){
-						maxType = jobFields2[0];
-						maxCores = cores;
-					}
-					if(maxType.equals(jobFields2[0])) n = Integer.parseInt(jobFields2[1]);
-				}
-				n++;
-				serverResponse=sendAndReceive(dout, in, "OK\n");
-				firstTime = false;
-			}
-	
-			if(serverResponse2.contains("JOBN")){
-				// Schedule the job to the server type with the maximum cores
-				serverResponse=sendAndReceive(dout, in, "SCHD "+jobID+" "+maxType+" "+jobID%n+"\n");
-				jobID++;
-			}
-			else serverResponse = serverResponse2;
-		}
-	
-		send(dout, "QUIT\n");
-		dout.close();
-		s.close();
-	}
-
-	//A method to send a message and receive a response
-	public static String sendAndReceive(DataOutputStream dout, BufferedReader in, String message) throws IOException {
-		send(dout, message);
-		return receive(in);
-	}
-
-	//A method to send a message
-	public static void send(DataOutputStream dout, String message) throws IOException {
-		dout.write(message.getBytes());
-		dout.flush();
-	}
-
-	//A method to receive a response
-	public static String receive(BufferedReader in) throws IOException {
-		return in.readLine();
-	}
-
-    //A method to find the largest server type based on cores
-    public static String findLargestType(BufferedReader in, int nRecs) throws IOException {
+class MyClient {
+    public static void main(String args[]) throws Exception {
+        Socket socket = new Socket("localhost", 50000);
+        DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+        BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        
+        // Connect to the server and authenticate
+        String serverResponse = sendAndReceive(output, input, "HELO\n");
+        String user = System.getProperty("user.name");
+        serverResponse = sendAndReceive(output, input, "AUTH " + user + "\n");
+        
+        // Initialize some variables for scheduling jobs
+        int jobID = 0;
+        int cores = 0;
+        int maxCores = 0;
+        int nRecs = 0;
+        int n = 0;
         String maxType = "";
-        int maxCores = 0, n=0;
-        for (int i = 0; i < nRecs; i++) {
-            String[] array2 = null;
-            String str = in.readLine();
-            array2 = str.split(" ");
-            int cores = Integer.parseInt(array2[4]);
-            if (cores > maxCores) {
-                maxType = array2[0];
-                maxCores = cores;
+        String serverResponse2 = "";
+        String[] jobFields = null;
+        boolean firstTime = true;
+        
+        // Loop until there are no more jobs
+        while (!serverResponse.equals("NONE")) {
+            // Request a job from the server
+            serverResponse2 = sendAndReceive(output, input, "REDY\n");
+            
+            // If this is the first time, get all the jobs and find the type with the most cores
+            if (firstTime) {
+                serverResponse = sendAndReceive(output, input, "GETS All\n");
+                jobFields = serverResponse.split(" ");
+                nRecs = Integer.parseInt(jobFields[1]);
+                send(output, "OK\n");
+                
+                // Loop through all the jobs and compare their cores
+                for (int i = 0; i < nRecs; i++) {
+                    String[] jobFields2 = null;
+                    serverResponse = receive(input);
+                    jobFields2 = serverResponse.split(" ");
+                    cores = Integer.parseInt(jobFields2[4]);
+                    if (cores > maxCores) {
+                        maxType = jobFields2[0];
+                        maxCores = cores;
+                    }
+                    if (maxType.equals(jobFields2[0]))
+                        n = Integer.parseInt(jobFields2[1]);
+                }
+                n++;
+                serverResponse = sendAndReceive(output, input, "OK\n");
+                firstTime = false;
             }
-            if (maxType.equals(array2[0])) n = Integer.parseInt(array2[1]);
+            
+            // If the server sent a new job, schedule it to the type with the most cores
+            if (serverResponse2.contains("JOBN")) {
+                serverResponse = sendAndReceive(output, input,
+                        "SCHD " + jobID + " " + maxType + " " + jobID % n + "\n");
+                jobID++;
+            } else
+                serverResponse = serverResponse2;
         }
-        return maxType;
+        
+        // Quit the connection
+        send(output, "QUIT\n");
+        output.close();
+        socket.close();
     }
 
+    // Helper method to send a message to the server and receive a response
+    public static String sendAndReceive(DataOutputStream output, BufferedReader input, String message)
+            throws IOException {
+        send(output, message);
+        return receive(input);
+    }
+
+    // Helper method to send a message to the server
+    public static void send(DataOutputStream output, String message) throws IOException {
+        output.write(message.getBytes());
+        output.flush();
+    }
+
+    // Helper method to receive a message from the server
+    public static String receive(BufferedReader input) throws IOException {
+        return input.readLine();
+    }
 }
